f660a545a36cfd37e6404433f4b4793cbb03afdb
moxDroid Labs Inc <pritam.world@gmail.com>
2022-10-01 10:11:21 -0400

    More endpoints added

--- server.js
+++ server.js
@@ -11,7 +11,24 @@ app.use(express.urlencoded()) // for parsing application/x-www-form-urlencoded
 
 //http://localhost:3000/
 app.get('/', function (req, res) {
-    res.send("<h1>Home Page</h1>")
+    res.send("<h1>Home Page - Pritesh Patel</h1>")
+})
+
+
+//http://localhost:3000/profile
+app.post('/profile', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
+  
+//http://localhost:3000/admin
+app.get('/admin', (req, res) => {
+    res.send('Admin Homepage')
+})
+
+//http://localhost:3000/user/100
+app.get("/user/:id", (req, res)=> {
+    res.send(`User ID: ${req.params.id}`);
 })
 
 
